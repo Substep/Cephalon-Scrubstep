527c51ff326af03ce15f99fc9dee5b893b0f780a
Glitch (substep-bot) <none>
2018-04-26 21:48:10 +0000

    👊🍥 Updated with Glitch

--- index.js
+++ index.js
@@ -18,4 +18,25 @@ client.on('message', message => {
 }
 });
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
+if(message.content.startsWith(config.prefix + "prefix")) {
+  // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+  let newPrefix = message.content.split(" ").slice(1, 2)[0];
+  // change the configuration in memory
+  config.prefix = newPrefix;
+
+  // Now we have to save the file.
+  fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+}
+
 client.login(process.env.TOKEN);
