28e201ae736ea414094b15163dc5ec3eb9f0fe59
Glitch (substep-bot) <none>
2018-04-26 22:29:08 +0000

    😜⛹ Updated with Glitch

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on("message", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
   if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
+    message.channel.send("pong");
   } else
   if (message.content.startsWith(config.prefix + "foo")) {
     message.channel.send("bar!");
