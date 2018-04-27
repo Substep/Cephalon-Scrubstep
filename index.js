18dcdac2e2a4eb404e95113e8d7f67f4eecfb38d
Glitch (substep-bot) <none>
2018-04-27 00:54:37 +0000

    🎂🌵 Updated with Glitch

--- index.js
+++ index.js
@@ -15,36 +15,27 @@ client.on('message', message => {
     var message2 = ", my name is Scrubstep.";
     var name = message.content.replace(/i am/gi, "");
     message.channel.send(message1 + name + message2);
-}
-});
-
-client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong");
-  } else
-  if (message.content.startsWith(config.prefix + "foo")) {
-    message.channel.send("bar!");
   }
 });
 
 client.on("message", (message) => {
-  if(message.content.startsWith(config.prefix + "prefix")) {
-    const modRole = message.guild.roles.find("name", "Owner");
-  if (!modRole) 
-    return console.log("The Owner role does not exist.");
-
-  if (!message.member.roles.has(modRole.id))
-    return message.send("You can't use this command.");
-
-      let newPrefix = message.content.split(" ").slice(1, 2)[0];
-      config.prefix = newPrefix;
-      var beg = "Sucessfully changed the prefix to";
-      var prefix = config.prefix;
-      fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
-      return message.send(beg.concat(prefix))
-}
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  
+  if (message.content.toLowerCase().startsWith(config.prefix + "ping")) {
+    message.channel.send({embed: {
+    "color": 3447003,
+    "timestamp": new Date(),
+    "footer": {
+      "icon_url": client.user.avatarURL,
+      "text": client.user.username
+    },
+    "fields": [
+      {
+        "name": "Ping Calculator",
+        "value": new Date().getTime() - message.createdTimestamp + " ms"
+      }
+    ]
+  }
 });
 
 client.login(process.env.TOKEN);
